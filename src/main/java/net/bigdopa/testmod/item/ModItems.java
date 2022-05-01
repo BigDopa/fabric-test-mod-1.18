035ecdb6e44a2ccf65ba9aad9b645b38ad23eec3
BigDopa <79546653+BigDopa@users.noreply.github.com>
2022-05-01 15:56:00 -0700

    Added test block glizzy_dog with custom pixel art

---
+++ src/main/java/net/bigdopa/testmod/item/ModItems.java
@@ -0,0 +1,22 @@
+package net.bigdopa.testmod.item;
+
+import net.bigdopa.testmod.TestMod;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+public class ModItems
+{
+    public static final Item GLIZZY_DOG = registerItem("glizzy_dog",
+            new Item(new FabricItemSettings().group(ItemGroup.MISC)));
+    private static Item registerItem(String name, Item item)
+    {
+        return Registry.register(Registry.ITEM, new Identifier(TestMod.MOD_ID, name), item);
+    }
+  public static void regsiterModItems()
+  {
+      TestMod.LOGGER.info("Registering mod items for " + TestMod.MOD_ID);
+  }
+}
