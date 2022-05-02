5da2739e396676c4f99b22b95e953b5effab9dba
BigDopa <79546653+BigDopa@users.noreply.github.com>
2022-05-02 11:11:35 -0700

    added recipes for cheese block and ingot, as well as adding raw cheese ore with texture

--- src/main/java/net/bigdopa/testmod/item/ModItems.java
+++ src/main/java/net/bigdopa/testmod/item/ModItems.java
@@ -13,6 +13,9 @@ public class ModItems
 
     public static final Item CHEESE_INGOT = registerItem("cheese_ingot",
             new Item(new FabricItemSettings().group(ModIemGroup.CHEESE)));
+
+    public static final Item RAW_CHEESE_ORE = registerItem("raw_cheese_ore",
+            new Item(new FabricItemSettings().group(ModIemGroup.CHEESE)));
     private static Item registerItem(String name, Item item)
     {
         return Registry.register(Registry.ITEM, new Identifier(TestMod.MOD_ID, name), item);
